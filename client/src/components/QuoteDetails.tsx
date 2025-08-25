931b149f6c2cff114304884dd04f4b40f9e57663
elaylevy123 <elaylevi2@gmail.com>
2025-08-25 22:14:27 +0300

    deploy

--- client/src/components/QuoteDetails.tsx
+++ client/src/components/QuoteDetails.tsx
@@ -145,8 +145,8 @@ const QuoteDetails: React.FC<QuoteDetailsProps> = ({ quoteId, onBack }) => {
           {/* תמונה גדולה מעל פרטי האירוע */}
           <div className="text-center mb-8">
             <img src="/pdf1.png" alt="header-img" style={{ 
-              maxWidth: '700px',
-              width: '300%',
+              maxWidth: '900px',
+              width: '500%',
               height: 'auto',
               margin: '0 auto'
             }} />
