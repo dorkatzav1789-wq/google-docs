7609c365f63aa4c8055ea4d8765dd320eec420b3
elaylevy123 <elaylevi2@gmail.com>
2025-08-26 18:39:47 +0300

    deploy

--- client/src/components/QuoteDetails.tsx
+++ client/src/components/QuoteDetails.tsx
@@ -366,7 +366,7 @@ const QuoteDetails: React.FC<QuoteDetailsProps> = ({ quoteId, onBack }) => {
                 <img src="/pdf4.png" alt="header-img" style={{
                   maxWidth: '620px',
                   height: 'auto',
-                  marginLeft: '20px',
+                  marginLeft: '10px',
                   marginRight: 'auto'
                 }}/>
               </div>
