20aee075b8c1a6dbfda03f8602947686d7fd5d71
elaylevy123 <elaylevi2@gmail.com>
2025-10-16 01:23:35 +0300

    Initial commit

--- client/src/components/ClientSelector.tsx
+++ client/src/components/ClientSelector.tsx
@@ -52,6 +52,7 @@ const ClientSelector: React.FC<ClientSelectorProps> = ({
         />
       </div>
 
+
       {loading ? (
         <div className="text-center py-4">
           <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600 mx-auto"></div>
